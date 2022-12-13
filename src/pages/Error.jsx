12f73f974bc7c1fb15063a27c9517cb080a25b47
Junaidhkn <junaidhkniazi@gmail.com>
2022-12-14 00:12:28 +0500

    error Element Added

--- src/pages/Error.jsx
+++ src/pages/Error.jsx
@@ -1,12 +1,15 @@
 import React from 'react'
+import { useRouteError } from 'react-router-dom'
 import MainNavigation from '../components/MainNavigation.jsx'
 
 const Error = () => {
+   const error = useRouteError()
    return (
       <>
          <MainNavigation />
          <main id="error-content">
             <h1>An Error Occurred!</h1>
+            <p>{error.message}</p>
          </main>
       </>
    )
